31a9780afc44ad7ea6dfbb9331a3207e7d03c532
clfhhc <clfhhc@gmail.com>
2020-12-28 11:53:29 -0800

    added typescript and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports =  {
+  parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
+  extends:  [
+    'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from @typescript-eslint/eslint-plugin
+    'prettier/@typescript-eslint',  // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'plugin:prettier/recommended',  // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
+  parserOptions:  {
+    ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
+    sourceType:  'module',  // Allows for the use of imports
+  },
+  env: {
+    node: true,
+    jest: true
+  },
+  rules:  {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. '@typescript-eslint/explicit-function-return-type': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', {
+      'vars': 'all',
+      'args': 'after-used',
+      'ignoreRestSiblings': false
+    }],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/no-namespace': 'off',
+    'no-bitwise': 'off'
+  },
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+  ],
+  settings:  {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts'],
+      },
+    },
+  },
+};
---
+++ src/index.js
@@ -0,0 +1,3 @@
+require('@babel/register')({ extensions: ['.js', '.ts'] });
+// import all other files after the babel hook
+// const app = require('./app');
